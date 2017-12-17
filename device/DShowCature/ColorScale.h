b3260768db042c23d3cd522d44ecbc2725e8a5bd
wwl33695 <wwl33695@126.com>
2017-12-17 18:27:37 +0800

    delete

--- device/DShowCature/ColorScale.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COLORSCALE_H__
-#define __COLORSCALE_H__
-#include <windef.h>
-
-bool  RGB2YUV420(LPBYTE RgbBuf,UINT nWidth,UINT nHeight,LPBYTE yuvBuf,unsigned long *len);
-
-bool YUV4202RGB(LPBYTE yuvBuf,UINT nWidth,UINT nHeight,LPBYTE pRgbBuf,unsigned long *len);
-
-#endif
