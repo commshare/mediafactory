2ad185e33d9b9dec9617ed9192c88d112c2f427c
wwl33695 <wwl33695@126.com>
2018-07-11 16:41:14 +0800

    new tsmux

---
+++ file/libmpeg/ts/ts_test.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>  
+#include <sstream>
+#include <unistd.h>
+
+#include "aacdemux.h"
+#include "h264demux.h"
+#include "tsmux2.h"
+
+int main()
+{
+	void* handle = AACDemux_Init("song.aac", 1);
+	void* h264handle = H264Demux_Init("ts.h264", 1);
+	if( !h264handle )
+	{
+		printf("H264Framer_Init error\n");
+		return -1;
+	}
+
+	void* tshandle = es2ts_alloc("test.ts");
+
+	const char *frame = NULL;
+	int length = 0;
+	const char *h264frame = NULL;
+	int framelength = -1;
+
+	int filecounter = 0;
+	int keyframecounter = 0;
+	std::stringstream ss;
+
+	uint64_t pts = 0;
+	int ret = 0;
+	while( 1 )
+	{
+		ret = AACDemux_GetFrame(handle, &frame, &length);
+		if( ret < 0 )
+			break;
+
+		ret = H264Demux_GetFrame(h264handle, &h264frame, &framelength);
+		if( ret < 0 )
+		{
+			printf("ReadOneNaluFromBuf error\n");
+			break;
+		}
+		int frametype = h264frame[4]&0x1F;
+		printf("frametype %d\n", frametype);
+		es2ts_writeH264(tshandle, h264frame, framelength, frametype == 7, pts);
+
+		pts += 3600;
+		
+		usleep(100 * 1000);
+	}
+
+	es2ts_destroy(tshandle);
+	return 0;
+}
---
+++ file/libmpeg/ts/tsmux2.h
@@ -0,0 +1,20 @@
+#ifndef __TSMUX2_H__
+#define __TSMUX2_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <fstream>
+
+//////////////////////////////////////////////////////////////////
+void *es2ts_alloc(const char* filename);
+
+/* 
+ *@remark: 整体发送数据的抽象逻辑处理函数接口 
+ */  
+int es2ts_writeH264(void *handle, const char* framedata, int nFrameLen, int keyframe, uint64_t timestamp);
+
+int es2ts_destroy(void *handle);
+
+#endif
